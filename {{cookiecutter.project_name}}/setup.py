6a4b2ff9c9722ab894469bd48b4bc18bbe587499
pbegle <peter.begle@bytecode.io>
2021-02-09 13:21:28 -0800

    Changed setup.py file to make it a little more dynamic.

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -1,24 +1,25 @@
-#!/usr/bin/env python
-from setuptools import setup
+from setuptools import find_packages, setup
+
+requirements_file = "requirements.txt"
+
+with open("README.md") as f:
+    readme = f.read()
 
 setup(
     name="{{cookiecutter.project_name}}",
     version="0.1.0",
-    description="Singer.io tap for extracting data",
+    description="{{cookiecutter.package_description}}",
+    long_description=readme,
     author="Stitch",
     url="http://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["{{cookiecutter.package_name}}"],
-    install_requires=[
-        # NB: Pin these to a more specific version for tap reliability
-        "singer-python",
-        "requests",
-    ],
+    install_requires=open(requirements_file).readlines(),
     entry_points="""
     [console_scripts]
     {{cookiecutter.project_name}}={{cookiecutter.package_name}}:main
     """,
-    packages=["{{cookiecutter.package_name}}"],
+    packages=find_packages(exclude=["tests"]),
     package_data = {
         "schemas": ["{{cookiecutter.package_name}}/schemas/*.json"]
     },
